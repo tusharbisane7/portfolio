be595be8e191c775abea810f241391cf0f2911fe
TUSHAR P BISANE <tpbisane7@gmail.com>
2025-06-08 20:06:45 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Tushar Bisane </span>
+            from <span className="purple"> Nagpur, India.</span>
+            <br />
+       🚀 Final-year B.Tech student in Information Technology  
+            <br />
+           💻 MERN Stack | Python | Java | SQL | DSA 
+            <br /> 🏆 300+ Coding Problems Solved 
+
+
+            <br />🎯 Always learning. Always building. Always growing.
+          <br/>  Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Cricket
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer"></footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
